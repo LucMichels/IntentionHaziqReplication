398a62f46c5496c9014067782e07a5262bc3420e
LucMichels <36677739+LucMichels@users.noreply.github.com>
2021-03-19 15:12:44 +0100

    new annotations

--- download.py
+++ download.py
@@ -1,6 +1,6 @@
 import gdown
 
-file_id = "1xRmRoeRfAzqn3v5bTEnsopEZgpdSJHV0"
+file_id = "1hVv07o7EabJUQS67UYSHzq0ro_OBFc-5"
 url = f'https://drive.google.com/uc?id={file_id}'
 output = 'luc_data.zip'
 gdown.download(url, output, quiet=False)
