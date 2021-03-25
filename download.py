3c87de0b7ed3c772ceaa0e5e672a423f1acf4667
LucMichels <36677739+LucMichels@users.noreply.github.com>
2021-03-25 15:11:45 +0100

    train on actual labdas training

--- download.py
+++ download.py
@@ -1,6 +1,6 @@
 import gdown
 
-file_id = "1UQj3q77GYiMfeHBJ-kwP26-RKMhU1QOH"
+file_id = "1Md2sI8gG6EFj__d4qoGb3S0_vTJ5MvkU"
 url = f'https://drive.google.com/uc?id={file_id}'
 output = 'luc_data.zip'
 gdown.download(url, output, quiet=False)
