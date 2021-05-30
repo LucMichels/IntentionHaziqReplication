97feae99e0d54036cdf815dc0adf6834915ca457
LucMichels <36677739+LucMichels@users.noreply.github.com>
2021-05-30 11:12:11 +0200

    downloading zip file

--- download.py
+++ download.py
@@ -1,6 +1,6 @@
 import gdown
 
-file_id = "1Md2sI8gG6EFj__d4qoGb3S0_vTJ5MvkU"
+file_id = "1W-YVkdBJU74xveTf97uo784gZIAw9aHe"
 url = f'https://drive.google.com/uc?id={file_id}'
 output = 'luc_data.zip'
 gdown.download(url, output, quiet=False)
